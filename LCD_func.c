6ef7f7cafa85a1feb9a7019f14c982eb0d783fb6
Mahmoud Helmy <52659572+mahmoudhelmyy@users.noreply.github.com>
2021-06-06 05:03:07 +0200

    Create delay n milliseconds Function and delay n microseconds Function

--- LCD_func.c
+++ LCD_func.c
@@ -80,4 +80,20 @@ for (y = 0; y < size; y++)
 }
 
 }
+//============================== delay n milliseconds (16 MHz CPU clock) ==============================
+void delayMs(int n)
+{
+int i, j;
+for(i = 0 ; i< n; i++) 
+for(j = 0; j <3180; j++)
+{} //do nothing for 1 ms
+}
 
+//============================== delay n microseconds (16 MHz CPU clock)==============================
+void delayUs(int n)
+{
+int i, j; 
+for(i = 0 ;i < n; i++) 
+for(j = 0; j < 3; j++)
+{} //do nothing for 1 us
+}
