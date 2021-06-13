d7f5d5a24bb55a0e841be5812f86db9a9a22cc00
AYAELHUSSENY <67708664+AYAELHUSSENY@users.noreply.github.com>
2021-06-13 23:52:33 +0200

    Parsing

--- main.c
+++ main.c
@@ -35,12 +35,48 @@ void delayUs(int n);
 void PORTF_init(void);
 void LED_ON(uint32_t distance);
 //============================== MAIN FUNCTION ==============================
-int main(void)
-{
-SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2) );
+	int main(void)
+	{
 PORTF_init();
 LCD_init();
 LED_ON(10); 
+		
+//============================== Parsing ==============================
+while(1){
+for(i=0;i<150;i++)
+{
+	Buffer[i] = UART1_Read();
+}
+temp = strstr(Buffer,"$GPGGA");         // find GGA data inside GPS data block
+if(*temp=='$')                         // start of a line
+{                 
+	i=0;
+	k=0;
+	while(temp[j]!='\x0a')       // end of a line
+	{     
+		if(temp[j]!=',')
+		{
+			data[i][k] = temp[j];
+			k++;
+		}
+		else
+		{
+			data[i][k] = '\0';
+			i++;
+			k=0;
+		}
+		j++;
+	}
+	j=0;
+}
+
+Latitude[0]=atof(data[2]);
+Longitude[0]=atof(data[4]);
+
+distance = distance_calc(30.244776028800565, 31.461346753073578,30.244975300103558, 31.463567622130945);
+LED_ON(distance);
+}		
+		
 
     }  
 
