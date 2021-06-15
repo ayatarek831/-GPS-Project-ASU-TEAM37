d1f88687786ffa88e0b4007c82153d54a69acafc
Mahmoud Yasser <84890294+mahmoudyasserr@users.noreply.github.com>
2021-06-15 03:09:23 +0200

    Removed Prototypes (Added in header file)

--- LCD_func.c
+++ LCD_func.c
@@ -1,17 +1,4 @@
 #include "tm4c123gh6pm.h"
-#include <stdio.h>
-#include "stdint.h"
-#include <math.h>
-#include "string.h"
-#define PI 3.14159265359
-#define RS 0x20																																		 //PORTA BIT5 mask 
-#define RW 0x40 																																	 //PORTA BIT6 mask 
-#define EN 0x80																																		 //PORTA BIT7 mask
-void delayMs(int n);
-void delayUs(int n);
-void LCD_command(unsigned char command);
-void LCD_data(unsigned char data);
-void LCD_init(void);
 
 //============================== LCD Intialization PORTA & PORTB ==============================
 void LCD_init(void)
