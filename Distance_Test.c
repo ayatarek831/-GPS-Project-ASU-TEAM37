3fbd2cd3d55cde09ebed9e147e8eefbd48c67b50
Mahmoud Yasser <84890294+mahmoudyasserr@users.noreply.github.com>
2021-06-15 03:02:36 +0200

    Update LED_ON function

--- Distance_Test.c
+++ Distance_Test.c
@@ -1,17 +1,5 @@
 #include "tm4c123gh6pm.h"
-#include <stdio.h>
-#include "stdint.h"
-#include <math.h>
-#include "string.h"
-#define PI 3.14159265359
 
-//============================== Functions Prototypes ==============================
-void LCD_command(unsigned char command);
-void LCD_data(unsigned char data);
-void LCD_init(void);
-void LCD_STRING(char* str);
-void delayMs(int n);
-void delayUs(int n);
 //============================== Intialize PORTF ==============================
 void PORTF_init(void){                                                 
 SYSCTL_RCGCGPIO_R |= 0x20;			       //enable clock of Port F
@@ -24,24 +12,27 @@ GPIO_PORTF_DEN_R |= 0x0E;              //enable PF1,PF2,PF3 (0000 1110)
 GPIO_PORTF_DIR_R |= 0x0E;              //enable PF1-PF3 as output (0000 1110)
 GPIO_PORTF_PCTL_R &= 0x00000000;       //PCTL GPIO
 }
-void LED_ON(uint32_t distance){
+void LED(int color){
+	GPIO_PORTF_DATA_R &= (~0x0E);
+	GPIO_PORTF_DATA_R |= color;
+}
+void LED_ON(double distance){
 char distance_str[32];
 if (distance >= 100)
 {
-	GPIO_PORTF_DATA_R |= 0x02 ;             //turn on RED LEDs (0000 0010)
+	LED(7);
 	LCD_command(1);                         //clear display 
 	LCD_command(0x80);                      //lcd cursor location
 	delayMs(500);
-	sprintf(distance_str, "The Distance is %d meter", distance);
+	sprintf(distance_str, "distance =      %f", distance);
 	LCD_STRING(distance_str);
-
 }
 else
-{	
-	GPIO_PORTF_DATA_R |= 0x00 ;                        //turn OFF LED
+{
+	LED(2);
 	LCD_command(1);                                    //clear display 
 	LCD_command(0x80);                                 //lcd cursor location 
-	sprintf(distance_str, "The Distance < 100 meter = %d meter", distance);
+	sprintf(distance_str, "distance =      %f", distance);
 	LCD_STRING(distance_str);
 }
 }
